81ecd718849a20168dcd40603ba517b3a7d817e8
Gustl22 <git@reb0.org>
2024-03-25 08:59:14 +0100

    create duration picker

--- packages/flutter/lib/material.dart
+++ packages/flutter/lib/material.dart
@@ -80,6 +80,7 @@ export 'src/material/dropdown.dart';
 export 'src/material/dropdown_menu.dart';
 export 'src/material/dropdown_menu_form_field.dart';
 export 'src/material/dropdown_menu_theme.dart';
+export 'src/material/duration_picker.dart';
 export 'src/material/elevated_button.dart';
 export 'src/material/elevated_button_theme.dart';
 export 'src/material/elevation_overlay.dart';
